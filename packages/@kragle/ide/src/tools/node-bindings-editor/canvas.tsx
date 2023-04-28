0ef909cd76440026b1ab7d2c5272a51925ef1b1b
Philipp Schiffmann <philippschiffmann93@gmail.com>
2023-04-28 13:22:23 +0200

    bezier curves node connection lines

---
+++ packages/@kragle/ide/src/tools/node-bindings-editor/canvas.tsx
@@ -0,0 +1,155 @@
+import { SceneDocument } from "@kragle/runtime";
+import { ReactElement } from "react";
+import { bemClasses } from "../../bem-classes.js";
+import { NodeBox } from "./node-box.js";
+import {
+  Layout,
+  NodeBoxPosition,
+  nodeBoxSizes,
+  useLayout,
+} from "./use-layout.js";
+
+const cls = bemClasses("node-bindings-canvas");
+
+export interface NodeBindingsCanvasProps {
+  document: SceneDocument;
+  zoom: number;
+}
+
+export function NodeBindingsCanvas({
+  document,
+  zoom,
+}: NodeBindingsCanvasProps) {
+  const layout = useLayout(document);
+
+  return (
+    <div className={cls.block()}>
+      <div
+        className={cls.element("container")}
+        style={{
+          width: layout.canvasWidth * zoom,
+          height: layout.canvasHeight * zoom,
+          transform: `scale(${zoom})`,
+        }}
+      >
+        <ConnectionLines document={document} layout={layout} />
+        {Object.entries(layout.nodeBoxPositions).map(([nodeId, position]) => {
+          const nodeJson = document.getNode(nodeId)!;
+          const { schema } = document.nodeDefinitions.get(nodeJson.type)!;
+          return (
+            <NodeBox
+              key={nodeId}
+              position={position}
+              nodeId={nodeId}
+              nodeJson={nodeJson}
+              schema={schema}
+            />
+          );
+        })}
+      </div>
+    </div>
+  );
+}
+
+interface ConnectionLinesProps {
+  document: SceneDocument;
+  layout: Layout;
+}
+
+function ConnectionLines({ document, layout }: ConnectionLinesProps) {
+  return (
+    <svg
+      className={cls.element("connection-lines")}
+      width={layout.canvasWidth}
+      height={layout.canvasHeight}
+    >
+      {Object.entries(layout.nodeBoxPositions).flatMap(
+        ([parentId, parentPosition]) => {
+          const lines: ReactElement[] = [];
+
+          function addLine(childId: string, childPosition: NodeBoxPosition) {
+            const x1 = parentPosition.offsetLeft + nodeBoxSizes.boxWidth;
+            const y1 =
+              parentPosition.offsetTop +
+              nodeBoxSizes.header / 2 +
+              nodeBoxSizes.padding;
+            const x2 = childPosition.offsetLeft;
+            const y2 =
+              childPosition.offsetTop +
+              nodeBoxSizes.header / 2 +
+              nodeBoxSizes.padding;
+            const center = (x1 + x2) / 2;
+            lines.push(
+              <path
+                key={childId}
+                className={cls.element("child-line")}
+                d={`M ${x1} ${y1} C ${center} ${y1} ${center} ${y2} ${x2} ${y2}`}
+              />
+            );
+          }
+
+          const nodeJson = document.getNode(parentId)!;
+          const { schema } = document.nodeDefinitions.get(nodeJson.type)!;
+          for (const childId of Object.values(nodeJson.slots)) {
+            if (!childId) continue;
+            addLine(childId, layout.nodeBoxPositions[childId]);
+          }
+          for (const slotName of schema.getCollectionSlots()) {
+            for (const childId of nodeJson.collectionSlots[slotName]) {
+              addLine(childId, layout.nodeBoxPositions[childId]);
+            }
+          }
+
+          return lines;
+        }
+      )}
+
+      {Object.entries(layout.nodeBoxPositions).flatMap(
+        ([targetId, targetPosition]) => {
+          const lines: ReactElement[] = [];
+
+          const targetJson = document.getNode(targetId)!;
+          for (const [inputName, targetInputOffset] of Object.entries(
+            targetPosition.inputOffsets
+          )) {
+            if (inputName.includes("/")) {
+              // TODO: collection input
+              continue;
+            } else {
+              const binding = targetJson.inputs[inputName];
+              if (binding?.type !== "node-output") continue;
+              const sourcePosition = layout.nodeBoxPositions[binding.nodeId];
+              const sourceOutputOffset =
+                sourcePosition.outputOffsets[binding.outputName];
+
+              const x1 =
+                sourcePosition.offsetLeft +
+                nodeBoxSizes.boxWidth +
+                nodeBoxSizes.connectorOffsetX;
+              const y1 =
+                sourcePosition.offsetTop +
+                sourceOutputOffset +
+                nodeBoxSizes.connectorOffsetY;
+              const x2 =
+                targetPosition.offsetLeft - nodeBoxSizes.connectorOffsetX;
+              const y2 =
+                targetPosition.offsetTop +
+                targetInputOffset +
+                nodeBoxSizes.connectorOffsetY;
+              const center = (x1 + x2) / 2;
+              lines.push(
+                <path
+                  key={`${targetId}/${inputName}`}
+                  className={cls.element("connection-line")}
+                  d={`M ${x1} ${y1} C ${center} ${y1} ${center} ${y2} ${x2} ${y2}`}
+                />
+              );
+            }
+          }
+
+          return lines;
+        }
+      )}
+    </svg>
+  );
+}
--- packages/@kragle/ide/src/tools/node-bindings-editor/index.tsx
+++ packages/@kragle/ide/src/tools/node-bindings-editor/index.tsx
@@ -1,13 +1,7 @@
 import { SceneDocument } from "@kragle/runtime";
-import { ReactElement } from "react";
+import { useState } from "react";
 import { bemClasses } from "../../bem-classes.js";
-import { NodeBox } from "./node-box.js";
-import {
-  Layout,
-  NodeBoxPosition,
-  nodeBoxSizes,
-  useLayout,
-} from "./use-layout.js";
+import { NodeBindingsCanvas } from "./canvas.js";
 
 const cls = bemClasses("bindings-editor");
 
@@ -16,131 +10,22 @@ interface NodeBindingsEditorProps {
 }
 
 export function NodeBindingsEditor({ document }: NodeBindingsEditorProps) {
-  const layout = useLayout(document);
+  const [zoom, setZoom] = useState(1);
   return (
     <div className={cls.block()}>
-      <div
-        className={cls.element("container")}
-        style={{ width: layout.canvasWidth, height: layout.canvasHeight }}
-      >
-        <ConnectionLines document={document} layout={layout} />
-        {Object.entries(layout.nodeBoxPositions).map(([nodeId, position]) => {
-          const nodeJson = document.getNode(nodeId)!;
-          const { schema } = document.nodeDefinitions.get(nodeJson.type)!;
-          return (
-            <NodeBox
-              key={nodeId}
-              position={position}
-              nodeId={nodeId}
-              nodeJson={nodeJson}
-              schema={schema}
-            />
-          );
-        })}
+      <NodeBindingsCanvas zoom={zoom} document={document} />
+      <div className={cls.element("canvas-shadow")} />
+      <div className={cls.element("zoom-control")}>
+        zoom:
+        <input
+          type="range"
+          min={0.1}
+          max={1}
+          step={0.05}
+          value={zoom}
+          onChange={(e) => setZoom(Number.parseFloat(e.target.value))}
+        />
       </div>
     </div>
-  );
-}
-
-interface ConnectionLinesProps {
-  document: SceneDocument;
-  layout: Layout;
-}
-
-function ConnectionLines({ document, layout }: ConnectionLinesProps) {
-  return (
-    <svg
-      className={cls.element("connection-lines")}
-      width={layout.canvasWidth}
-      height={layout.canvasHeight}
-    >
-      {Object.entries(layout.nodeBoxPositions).flatMap(
-        ([parentId, parentPosition]) => {
-          const lines: ReactElement[] = [];
-
-          function addLine(childId: string, childPosition: NodeBoxPosition) {
-            lines.push(
-              <line
-                key={childId}
-                className={cls.element("child-line")}
-                x1={parentPosition.offsetLeft + nodeBoxSizes.boxWidth}
-                y1={
-                  parentPosition.offsetTop +
-                  nodeBoxSizes.header / 2 +
-                  nodeBoxSizes.padding
-                }
-                x2={childPosition.offsetLeft}
-                y2={
-                  childPosition.offsetTop +
-                  nodeBoxSizes.header / 2 +
-                  nodeBoxSizes.padding
-                }
-              />
-            );
-          }
-
-          const nodeJson = document.getNode(parentId)!;
-          const { schema } = document.nodeDefinitions.get(nodeJson.type)!;
-          for (const childId of Object.values(nodeJson.slots)) {
-            if (!childId) continue;
-            addLine(childId, layout.nodeBoxPositions[childId]);
-          }
-          for (const slotName of schema.getCollectionSlots()) {
-            for (const childId of nodeJson.collectionSlots[slotName]) {
-              addLine(childId, layout.nodeBoxPositions[childId]);
-            }
-          }
-
-          return lines;
-        }
-      )}
-
-      {Object.entries(layout.nodeBoxPositions).flatMap(
-        ([targetId, targetPosition]) => {
-          const lines: ReactElement[] = [];
-
-          const targetJson = document.getNode(targetId)!;
-          for (const [inputName, targetInputOffset] of Object.entries(
-            targetPosition.inputOffsets
-          )) {
-            if (inputName.includes("/")) {
-              // TODO: collection input
-              continue;
-            } else {
-              const binding = targetJson.inputs[inputName];
-              if (binding?.type !== "node-output") continue;
-              const sourcePosition = layout.nodeBoxPositions[binding.nodeId];
-              const sourceOutputOffset =
-                sourcePosition.outputOffsets[binding.outputName];
-
-              lines.push(
-                <line
-                  key={`${targetId}/${inputName}`}
-                  className={cls.element("connection-line")}
-                  x1={
-                    sourcePosition.offsetLeft +
-                    nodeBoxSizes.boxWidth +
-                    nodeBoxSizes.connectorOffsetX
-                  }
-                  y1={
-                    sourcePosition.offsetTop +
-                    sourceOutputOffset +
-                    nodeBoxSizes.connectorOffsetY
-                  }
-                  x2={targetPosition.offsetLeft - nodeBoxSizes.connectorOffsetX}
-                  y2={
-                    targetPosition.offsetTop +
-                    targetInputOffset +
-                    nodeBoxSizes.connectorOffsetY
-                  }
-                />
-              );
-            }
-          }
-
-          return lines;
-        }
-      )}
-    </svg>
   );
 }
