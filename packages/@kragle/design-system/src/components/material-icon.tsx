674e377acb080c200e0ff828ab8690e72d98d568
Philipp Schiffmann <philippschiffmann93@gmail.com>
2023-06-25 13:49:31 +0200

    use material icons outlined variant

--- packages/@kragle/design-system/src/components/material-icon.tsx
+++ packages/@kragle/design-system/src/components/material-icon.tsx
@@ -1,12 +1,22 @@
+import { bemClasses } from "../index.js";
+
+const cls = bemClasses("kragle-material-icon");
+
 export interface MaterialIconProps {
   icon: string;
+  title?: string;
   className?: string;
 }
 
-export function MaterialIcon({ icon, className }: MaterialIconProps) {
+export function MaterialIcon({ icon, title, className }: MaterialIconProps) {
   return (
     <span
-      className={className ? `material-icons ${className}` : `material-icons`}
+      className={cls.block(
+        className
+          ? `material-icons-outlined ${className}`
+          : `material-icons-outlined`
+      )}
+      title={title}
     >
       {icon}
     </span>
