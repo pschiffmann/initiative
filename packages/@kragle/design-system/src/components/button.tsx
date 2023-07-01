1d385eb7f30a3f033bacdd3c57bbc5c5044e5c5f
Philipp Schiffmann <philippschiffmann93@gmail.com>
2023-07-01 23:35:52 +0200

    button disabled prop

--- packages/@kragle/design-system/src/components/button.tsx
+++ packages/@kragle/design-system/src/components/button.tsx
@@ -4,13 +4,18 @@ const cls = bemClasses("kragle-button");
 
 export interface ButtonProps {
   label: string;
+  disabled?: boolean;
   onPress?(): void;
   className?: string;
 }
 
-export function Button({ label, onPress, className }: ButtonProps) {
+export function Button({ label, disabled, onPress, className }: ButtonProps) {
   return (
-    <button className={cls.block(className)} onClick={onPress}>
+    <button
+      className={cls.block(className)}
+      disabled={disabled}
+      onClick={onPress}
+    >
       <span className={cls.element("label")}>{label}</span>
     </button>
   );
