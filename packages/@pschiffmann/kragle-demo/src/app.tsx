9740dd18197eceedd18d47b9eda635c170793dfd
Brickless1 <97314129+Brickless1@users.noreply.github.com>
2023-07-09 12:54:00 +0200

    clean up

--- packages/@pschiffmann/kragle-demo/src/app.tsx
+++ packages/@pschiffmann/kragle-demo/src/app.tsx
@@ -2,12 +2,9 @@ import sceneJson from "#kragle/scenes/article-management/scene.json";
 import {
   Scene,
   SceneRuntime,
-  generateCodeForScene,
   resolveDefinitions,
   sceneDocumentFromJson,
 } from "@kragle/runtime";
-
-import { Articlemanagement } from "../kragle/scenes/article-management/code-gen-test-output.js";
 
 const [, definitions] = resolveDefinitions([
   ["#kragle/libraries/index.js", await import("#kragle/libraries/index.js")],
@@ -22,7 +19,5 @@ console.log({ definitions, document, errors });
 const runtime = new SceneRuntime(document!);
 
 export function App() {
-  console.log(generateCodeForScene(document!));
-  return <Articlemanagement />;
   return <Scene runtime={runtime} />;
 }
