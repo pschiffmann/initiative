6f72b51ff33e2208dcd472c1cebcefdb90c58b58
Philipp Schiffmann <philippschiffmann93@gmail.com>
2023-09-22 01:09:11 +0200

    Data flow inspector
    Co-authored-by: Brickless1 <97314129+Brickless1@users.noreply.github.com>

--- packages/@initiativejs/ide/src/ide/tools/data-flow-inspector/index.tsx
+++ packages/@initiativejs/ide/src/ide/tools/data-flow-inspector/index.tsx
@@ -1,16 +1,24 @@
 import { Dialog, DialogCommand, bemClasses } from "#design-system";
+import { SceneDocument } from "#shared";
 import { CommandController } from "@initiativejs/react-command";
+import { Canvas } from "./canvas.js";
 
 const cls = bemClasses("initiative-data-flow-inspector");
 
 export interface DataFlowInspectorProps {
   controller: CommandController<DialogCommand>;
+  document: SceneDocument;
+  selectedNode: string | null;
 }
 
-export function DataFlowInspector({ controller }: DataFlowInspectorProps) {
+export function DataFlowInspector({
+  controller,
+  document,
+  selectedNode,
+}: DataFlowInspectorProps) {
   return (
     <Dialog className={cls.block()} commandStream={controller}>
-      TODO
+      <Canvas document={document} selectedNode={selectedNode} />
     </Dialog>
   );
 }
---
+++ packages/@initiativejs/ide/src/ide/tools/data-flow-inspector/node-box.tsx
@@ -0,0 +1,128 @@
+import {
+  ColorSchemeContext,
+  IconButton,
+  Typography,
+  bemClasses,
+} from "#design-system";
+import { NodeData } from "#shared";
+import {
+  TonalPalette,
+  argbFromHex,
+  hexFromArgb,
+} from "@material/material-color-utilities";
+import { useContext, useMemo } from "react";
+import { NodeBoxPosition } from "./layout-algorithm.js";
+
+declare module "csstype" {
+  interface Properties {
+    "--initiative-data-flow-inspector-node-box-fill-color"?: string;
+    "--initiative-data-flow-inspector-node-box-stroke-color"?: string;
+  }
+}
+
+const cls = bemClasses("initiative-data-flow-inspector-node-box");
+
+export interface NodeBoxProps {
+  data: NodeData;
+  focus: string | null;
+  positioning: NodeBoxPosition;
+}
+
+export function NodeBox({ data, focus, positioning }: NodeBoxProps) {
+  const schema = data.schema;
+
+  const colorScheme = useContext(ColorSchemeContext);
+  const style = useMemo(() => {
+    const color = data.schema.editor?.color;
+    if (!color) return undefined;
+    const customPalette = TonalPalette.fromInt(argbFromHex(color));
+    return {
+      "--initiative-data-flow-inspector-node-box-fill-color": hexFromArgb(
+        customPalette.tone(colorScheme !== "light" ? 20 : 80),
+      ),
+      "--initiative-data-flow-inspector-node-box-stroke-color": hexFromArgb(
+        customPalette!.tone(colorScheme !== "light" ? 80 : 20),
+      ),
+    };
+  }, [colorScheme]);
+
+  return (
+    <div
+      className={cls.block()}
+      style={{
+        top: positioning.offsetTop,
+        left: positioning.offsetLeft,
+        ...style,
+      }}
+    >
+      <Typography className={cls.element("id")} variant="title-medium" noWrap>
+        {data.id}
+      </Typography>
+      <Typography className={cls.element("type")} variant="label-medium" noWrap>
+        {data.type}
+      </Typography>
+      {data.forEachInput((expression, type, inputName, index) => {
+        const inputKey =
+          index === undefined ? inputName : `${inputName}::${index}`;
+        return <Input key={inputKey} name={inputKey} />;
+      })}
+      {schema.forEachOutput((outputName) => (
+        <Output key={outputName} name={outputName} />
+      ))}
+    </div>
+  );
+}
+
+interface InputOutputProps {
+  name: string;
+}
+
+function Input({ name }: InputOutputProps) {
+  return (
+    <>
+      <IconButton
+        className={cls.element("input-socket")}
+        label="Connect input"
+        icon="login"
+      />
+      <Typography
+        variant="body-medium"
+        noWrap
+        className={cls.element("input-name")}
+      >
+        {name}
+      </Typography>
+      <IconButton
+        className={cls.element("input-button")}
+        label="Edit input"
+        icon="edit"
+        disabled
+      />
+    </>
+  );
+}
+
+function Output({ name }: InputOutputProps) {
+  return (
+    <>
+      <IconButton
+        className={cls.element("output-socket")}
+        label="Connect input"
+        icon="logout"
+      />
+      <Typography
+        variant="body-medium"
+        noWrap
+        className={cls.element("output-name")}
+      >
+        {name}
+      </Typography>
+      <IconButton
+        className={cls.element("output-button")}
+        label="Edit input"
+        icon="edit"
+        disabled
+      />
+    </>
+  );
+}
