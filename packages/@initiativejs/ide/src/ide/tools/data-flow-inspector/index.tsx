901d8fac0cd0e63eee24be2f1b6fac29c3bef548
Philipp Schiffmann <philippschiffmann93@gmail.com>
2023-09-23 00:17:21 +0200

    data flow inspector zoom and line hover

--- packages/@initiativejs/ide/src/ide/tools/data-flow-inspector/index.tsx
+++ packages/@initiativejs/ide/src/ide/tools/data-flow-inspector/index.tsx
@@ -1,6 +1,7 @@
 import { Dialog, DialogCommand, bemClasses } from "#design-system";
 import { SceneDocument } from "#shared";
 import { CommandController } from "@initiativejs/react-command";
+import { useState } from "react";
 import { Canvas } from "./canvas.js";
 
 const cls = bemClasses("initiative-data-flow-inspector");
@@ -16,9 +17,29 @@ export function DataFlowInspector({
   document,
   selectedNode,
 }: DataFlowInspectorProps) {
+  const [zoom, setZoom] = useState(100);
+
   return (
     <Dialog className={cls.block()} commandStream={controller}>
-      <Canvas document={document} selectedNode={selectedNode} />
+      <Canvas
+        className={cls.element("canvas")}
+        document={document}
+        selectedNode={selectedNode}
+        zoom={zoom / 100}
+      />
+
+      <label className={cls.element("controls")}>
+        Zoom:
+        <input
+          className={cls.element("input")}
+          type="range"
+          min={1}
+          max={100}
+          step={1}
+          value={zoom}
+          onChange={(e) => setZoom(Number.parseInt(e.target.value))}
+        />
+      </label>
     </Dialog>
   );
 }
