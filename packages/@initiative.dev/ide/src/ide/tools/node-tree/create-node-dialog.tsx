1f35db04c45f4b45b20e1aafb7e89a3d938a697a
Philipp Schiffmann <philippschiffmann93@gmail.com>
2024-02-01 11:28:38 +0100

    node tree copy&paste
    Co-authored-by: Brickless1 <97314129+Brickless1@users.noreply.github.com>

--- packages/@initiative.dev/ide/src/ide/tools/node-tree/create-node-dialog.tsx
+++ packages/@initiative.dev/ide/src/ide/tools/node-tree/create-node-dialog.tsx
@@ -3,18 +3,25 @@ import {
   Button,
   Dialog,
   DialogCommand,
+  IconButton,
   SelectControl,
   TextFieldControl,
   bemClasses,
 } from "#design-system";
-import { SceneDocument } from "#shared";
+import {
+  ComponentNodeJson,
+  ExpressionJson,
+  NodeOutputExpressionJson,
+  SceneDocument,
+  SlotNodeJson,
+} from "#shared";
 import {
   CommandController,
   CommandStream,
   useAcceptCommands,
 } from "@initiative.dev/react-command";
 import { NodeSchema, validateNodeId } from "@initiative.dev/schema";
-import { useMemo, useState } from "react";
+import { useCallback, useMemo, useState } from "react";
 
 const cls = bemClasses("initiative-create-node-dialog");
 
@@ -65,12 +72,123 @@ export function CreateNodeDialog({
     controller.send("close");
   }
 
+  const testPaste = useCallback(() => {
+    //TODO:
+    // support mutiple base node strings
+    // support incorrectly formated strings
+    // support incorrect strings?
+    const x = navigator.clipboard.readText().then(function (data) {
+      const errors: string[] = [];
+
+      const bin: Record<string, ComponentNodeJson | SlotNodeJson> =
+        JSON.parse(data);
+
+      const pasteTreeParent: Map<string, string> = new Map();
+      const pasteTreeSlot: Map<string, string> = new Map();
+      const pasteTrueSelf: Map<string, string> = new Map();
+      //nodes
+      for (const id of Object.keys(bin)) {
+        const finalId: string = document.hasNode(id)
+          ? document.generateNodeId(id)
+          : id;
+        pasteTrueSelf.set(id, finalId);
+
+        if (bin[id].type === "SceneSlot") {
+          document.applyPatch({
+            type: "create-slot-node",
+            debugPreview: (bin[id] as SlotNodeJson).debugPreview,
+            parent: pasteTreeParent.has(id)
+              ? {
+                  nodeId: pasteTreeParent.get(id)!,
+                  slotName: pasteTreeSlot.get(id)!,
+                }
+              : parentId
+              ? { nodeId: parentId, slotName }
+              : undefined,
+            nodeId: finalId,
+          });
+          continue;
+        }
+
+        for (const [childSlot, childId] of Object.entries(
+          (bin[id] as ComponentNodeJson).slots,
+        )) {
+          pasteTreeParent.set(childId, finalId);
+          pasteTreeSlot.set(childId, childSlot.split("::")[0]);
+        }
+
+        document.applyPatch({
+          type: "create-component-node",
+          nodeType: bin[id].type,
+          parent: pasteTreeParent.has(id)
+            ? {
+                nodeId: pasteTreeParent.get(id)!,
+                slotName: pasteTreeSlot.get(id)!,
+              }
+            : parentId
+            ? { nodeId: parentId, slotName }
+            : undefined,
+          nodeId: finalId,
+        });
+      }
+      //inputs
+      for (const [id, node] of Object.entries(bin)) {
+        if (node.type === "SceneSlot") {
+          for (const [output, data] of Object.entries(
+            (node as SlotNodeJson).outputs,
+          )) {
+            try {
+              document.applyPatch({
+                type: "create-slot-node-output",
+                nodeId: pasteTrueSelf.get(id)!,
+                outputName: output,
+                outputType: data.type,
+                expression: data.value
+                  ? replaceNodeIds(data.value, pasteTrueSelf)
+                  : null,
+              });
+            } catch (e) {
+              errors.push(`- In node ${pasteTrueSelf.get(id)}: ${e}`);
+            }
+          }
+          continue;
+        }
+        for (const [input, connection] of Object.entries(
+          (node as ComponentNodeJson).inputs,
+        )) {
+          try {
+            document.applyPatch({
+              type: "set-component-node-input",
+              nodeId: pasteTrueSelf.get(id)!,
+              expression: replaceNodeIds(connection, pasteTrueSelf),
+              inputName: input.split("::")[0],
+              index: Number(input.split("::")[1]),
+            });
+          } catch (e) {
+            errors.push(`- In node ${pasteTrueSelf.get(id)}: ${e}`);
+          }
+        }
+      }
+      if (errors.length !== 0) {
+        window.alert("Encountered errors during import\n" + errors.join("\n"));
+      }
+    });
+
+    controller.send("close");
+  }, [slotName]);
+
   return (
     <Dialog className={cls.block()} commandStream={controller}>
       <AlertDialogContent
         title="New node"
         actions={
           <>
+            <IconButton
+              label="Paste node"
+              icon="content_paste"
+              disabled={false}
+              onPress={testPaste}
+            />
             <Button label="Cancel" onPress={() => controller.send("close")} />
             <Button
               className={cls.element("create-button")}
@@ -100,4 +218,42 @@ export function CreateNodeDialog({
       </AlertDialogContent>
     </Dialog>
   );
+}
+
+function replaceNodeIds(
+  data: ExpressionJson,
+  table: Map<string, string>,
+): ExpressionJson {
+  if (data.type === "fluent-message") {
+    const resultFluent: Record<string, ExpressionJson> = {};
+    Object.entries(data.args).forEach(([key, value]) => {
+      resultFluent[key] = replaceNodeIds(value, table);
+    });
+    return { ...data, args: resultFluent };
+  }
+  if (
+    data.type === "node-output" ||
+    data.type === "scene-input" ||
+    data.type === "debug-value"
+  ) {
+    const result = {
+      ...data,
+      selectors: data.selectors.map((value) => {
+        if (value.type === "property") return value;
+        return {
+          ...value,
+          args: value.args.map((nextLayerData) => {
+            return nextLayerData ? replaceNodeIds(nextLayerData, table) : null;
+          }),
+        };
+      }),
+    };
+    return data.type === "node-output"
+      ? {
+          ...(result as NodeOutputExpressionJson),
+          nodeId: table.get(data.nodeId) ?? data.nodeId,
+        }
+      : result;
+  }
+  return data;
 }
